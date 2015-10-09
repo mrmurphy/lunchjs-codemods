6be1416d899c7a2e73abe0b34bb200070d0eac1a
Murphy Randle <murphyrandle@gmail.com>
2015-10-08 22:28:16 -0600

    Add mod to change 'target' to 'son'

--- transform.js
+++ transform.js
@@ -8,10 +8,22 @@ function changeVarToLet(j, src) {
   })
 }
 
+/* Change all ocurrances of the variable 'target' to 'son' */
+function targetToSon(j, src) {
+  src.find(j.Identifier)
+  .filter(path => {
+    return path.value.name == "target"
+  })
+  .forEach(path => {
+    path.value.name = "son"
+  })
+}
+
 module.exports = function(fileInfo, api) {
   let j = api.jscodeshift
 
   let src = api.jscodeshift(fileInfo.source)
   changeVarToLet(j, src)
+  targetToSon(j, src)
   return src.toSource()
 }
